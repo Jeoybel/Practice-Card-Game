6cf2ec4e248b911f202248ca5e54cf189762a3cc
Jeoybel <57044026+Jeoybel@users.noreply.github.com>
2019-11-08 17:37:34 -0800

    Added instructions

--- src/GambleSwiftShuffle.cpp
+++ src/GambleSwiftShuffle.cpp
@@ -8,6 +8,15 @@ using namespace std;
 int main() {
 	string input{ "" };
 
+	cout << "Welcome to my card game!\n" << endl;
+
+	cout << "Instructions" << endl;
+	cout << "============" << endl;
+	cout << "You will be asked to remember the positions of several cards." << endl;
+	cout << "When all of the cards are flipped over several swaps will be made represented by [*]." << endl;
+	cout << "After all swaps are made recall as many cards as you can to score points." << endl;
+	cout << "There will be a total of three rounds. Good luck!\n" << endl;
+
 	while (input != "q") {													// Type 'q' to exit game loop
 		while (input != "easy" && input != "medium" && input != "hard") {	// Type difficulty to set game
 			cout << "(easy), (medium), (hard)" << endl;
@@ -18,7 +27,7 @@ int main() {
 			input = stringToLower(input);
 		}
 		CardGame cardGame(input);
-		cout << "Type 'q' to quit, otherwise continue!" << endl;
+		cout << "Enter 'q' to quit, otherwise enter anything to continue!" << endl;
 		cin >> input;
 		cin.clear();
 		cin.ignore(32767, '\n');
